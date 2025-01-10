27206f35f4e1cb022bc7f08f030827c82109d436
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-10 23:33:32 +0100

    Implemented all components (testing)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,50 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/Theme";
+import {
+  ButtonText,
+  MainTitle,
+  SectionSubtitle,
+  SectionTitle,
+  SubText,
+  Text,
+} from "./components/ui/Typography";
+import { GlobalStyles } from "./styles/Global";
+import {
+  ArrowLeft,
+  ArrowRight,
+  Heart,
+  Music,
+  Pause,
+  Play,
+  Search,
+  SkipLeft,
+  SkipRight,
+  Volume,
+} from "./components/ui/Icons.jsx";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <MainTitle>Hello</MainTitle>
+      <SectionTitle>Hello</SectionTitle>
+      <SectionSubtitle>Hello</SectionSubtitle>
+      <Text>Hello</Text>
+      <SubText>Hello</SubText>
+      <ButtonText>Hello</ButtonText>
+      <br />
+      <Play color="red" />
+      <SkipLeft color="blue" />
+      <SkipRight color="green" />
+      <Pause />
+      <Volume />
+      <Music />
+      <ArrowLeft />
+      <ArrowRight />
+      <Heart />
+      <Search />
+    </ThemeProvider>
+  );
 }
 
 export default App;
