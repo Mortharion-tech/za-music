801fad9d9670358b4441824dba7267e118c78548
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-10 23:35:53 +0100

    Implemented Typography

---
+++ src/components/ui/Typography.js
@@ -0,0 +1,65 @@
+import { styled } from "styled-components";
+
+export const MainTitle = styled.h1`
+  font-family: ${({ theme }) => theme.fonts.poppins}, sans-serif;
+  font-size: ${({ theme }) => theme.fontWeights.bold};
+  font-weight: 600;
+  line-height: 90px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+`;
+
+export const SectionTitle = styled.h2`
+  font-family: ${({ theme }) => theme.fonts.poppins}, sans-serif;
+  font-size: ${({ theme }) => theme.fontWeights.bold};
+  font-size: 35px;
+  line-height: 52.5px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+`;
+
+export const SectionSubtitle = styled.h3`
+  font-family: ${({ theme }) => theme.fonts.poppins}, sans-serif;
+  font-size: ${({ theme }) => theme.fontWeights.bold};
+  font-size: 25px;
+  font-weight: 600;
+  line-height: 37.5px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+`;
+
+export const Text = styled.span`
+  font-family: Inter, sans-serif;
+  font-size: ${({ theme }) => theme.fontWeights.bold};
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 26.63px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+`;
+
+export const SubText = styled.span`
+  font-family: Inter, sans-serif;
+  font-size: ${({ theme }) => theme.fontWeights.bold};
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 24.2px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+`;
+
+export const ButtonText = styled.span`
+  font-family: Inter, sans-serif;
+  font-size: ${({ theme }) => theme.fontWeights.bold};
+  font-size: 23px;
+  font-weight: 600;
+  line-height: 27.84px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+`;
