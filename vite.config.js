2b4a04f87bac784cc540418e7d4448dae05cea76
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-11 19:34:31 +0100

    Config Updates

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "@svgr/rollup";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,5 +14,10 @@ export default defineConfig({
   ],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
