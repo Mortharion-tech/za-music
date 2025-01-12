c79869370838334e0cf4d677cd6fd654da6df979
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-12 22:57:38 +0100

    Implemented Skeleton Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { SkeletonTheme } from "react-loading-skeleton";
 import { theme } from "@/styles/Theme";
 /* import {
   ButtonText,
@@ -24,12 +25,20 @@ import { GlobalStyles } from "@/styles/Global";
 import Home from "@/pages/Home";
 import Header from "@/components/Header";
 
+//  Import skeleton loader css
+import "react-loading-skeleton/dist/skeleton.css";
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Header />
-      <Home />
+      <SkeletonTheme
+        baseColor={theme.colors.secondaryBlack}
+        highlightColor={theme.colors.lightWhite}
+      >
+        <GlobalStyles />
+        <Header />
+        <Home />
+      </SkeletonTheme>
     </ThemeProvider>
   );
 }
