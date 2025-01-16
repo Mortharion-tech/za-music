33bb47f00333b3bcce25d3b4260d7be60fa01c54
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-16 18:55:20 +0100

    Implemented Search Function

---
+++ src/pages/Search/index.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import SearchIcon from "@/assets/icons/search.svg";
+import { InputWrapper, NotFoundText, TableTitle, Wrapper } from "./styled";
+import { search } from "@/services/api";
+import TracksTable from "@/components/TracksTable";
+import Input from "@/components/ui/Input";
+
+function Search() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tracks, setTracks] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const loadData = async () => {
+      //  TODO: Add debounce
+      try {
+        setIsLoading(true);
+        const data = await search(searchQuery);
+        setTracks(data);
+        console.log(data);
+      } catch (err) {
+        toast.error(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (searchQuery) loadData();
+  }, [searchQuery]);
+
+  return (
+    <Wrapper>
+      <InputWrapper>
+        <Input
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={(event) => setSearchQuery(event.target.value)}
+          startIcon={SearchIcon}
+        />
+      </InputWrapper>
+      {searchQuery && (
+        <div>
+          <TableTitle>Results by: {searchQuery}</TableTitle>
+          {tracks?.length > 0 && <TracksTable isLoading={isLoading} tracks={tracks} />}
+        </div>
+      )}
+      {searchQuery && !isLoading && tracks?.length <= 0 && (
+        <NotFoundText>Nothing was found :(</NotFoundText>
+      )}
+    </Wrapper>
+  );
+}
+
+export default Search;
