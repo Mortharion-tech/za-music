7e68c6a322827682cc3503759386265c4caffa09
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-21 20:51:57 +0100

    Implemented Artist API Request

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 const API_BASE_URL = "https://api.deezer.com";
 const API_CHART_URL = "/chart";
 const API_ALL_GENRES_URL = "/genre";
+const API_ALL_ARTISTS_URL = "/artist";
 const API_SEARCH_URL = "/search";
 const API_TOP_TRACKS_RADIO_URL = "/radio/37151/tracks";
 
@@ -10,7 +11,7 @@ export async function loadTopRadioTracks() {
   try {
     const data = await axios(`${API_TOP_TRACKS_RADIO_URL}?limit=100`);
     console.log("Radio API response:", data);
-    if (!data.data.data) throw Error();
+    if (!data?.data?.data) throw Error();
     console.log("Radio API response:", data.data.data);
     return data.data.data;
   } catch (err) {
@@ -21,7 +22,7 @@ export async function loadTopRadioTracks() {
 export async function loadCharts() {
   try {
     const data = await axios(API_CHART_URL);
-    if (!data.data) throw Error();
+    if (!data?.data) throw Error();
     console.log("Charts API response:", data.data);
     return data.data;
   } catch (err) {
@@ -33,7 +34,7 @@ export async function loadGenres() {
   try {
     const data = await axios.get(API_ALL_GENRES_URL);
     console.log("Genres API response:", data.data);
-    if (!data.data.data) throw Error();
+    if (!data?.data?.data) throw Error();
     return data.data.data.filter((genre) => genre.name.toLowerCase() !== "all");
   } catch (err) {
     throw Error("Failed to load genres!");
@@ -48,10 +49,10 @@ export async function loadGenre(genreId) {
     ]);
     console.log("Genre API response:", genreData, radiosData);
 
-    if (!genreData?.data || !radiosData?.data) throw Error();
+    if (!genreData?.data || !radiosData?.data?.data) throw Error();
 
-    const randomIndex = Math.floor(Math.random() * radiosData.data.data.length);
-
+    radios = radiosData.data.data;
+    const randomIndex = Math.floor(Math.random() * radios.length);
     const tracksData = await axios(radiosData[randomIndex].tracklist.replace(API_BASE_URL, ""));
 
     return {
@@ -63,11 +64,30 @@ export async function loadGenre(genreId) {
   }
 }
 
+export async function loadArtist(artistId) {
+  try {
+    const [artistData, tracksData] = await Promise.all([
+      axios.get(`${API_ALL_ARTISTS_URL}/${artistId}`),
+      axios.get(`${API_ALL_ARTISTS_URL}/${artistId}/top`),
+    ]);
+    console.log("Genre API response:", genreData, radiosData);
+
+    if (!artistData?.data || !tracksData?.data.data) throw Error();
+
+    return {
+      artist: artistData.data,
+      tracks: tracksData.data.data,
+    };
+  } catch (err) {
+    throw Error("Failed to load this artist!");
+  }
+}
+
 export async function search(searchQuery) {
   try {
     const data = await axios.get(`${API_SEARCH_URL}?q=${searchQuery}`);
     console.log("Search API response:", data.data.data);
-    if (!data.data.data) throw Error();
+    if (!data?.data?.data) throw Error();
     return data.data.data;
   } catch (err) {
     throw Error("Failed to load tracks!");
